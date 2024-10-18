e6682773dd09b2db470600d96db9b937d779e593
Finley <161162126+Finley224@users.noreply.github.com>
2024-10-17 22:55:52 -0400

    Add env config test
    moved over from the main branch

---
+++ testing/mod_test.ts
@@ -0,0 +1,15 @@
+import { assertEquals, assertNotEquals } from "@std/assert";
+import "jsr:@std/dotenv/load";
+
+Deno.test("testingEnvFileSetup", () => {
+    if (Deno.env.get("MEOWERTESTUSER") === undefined) {
+      console.warn("The env file is missing a username for testing");
+    }
+    if (Deno.env.get("MEOWERTESTPW") === undefined) {
+      console.warn("The env file is missing a password for testing");
+    }
+  
+    assertNotEquals(Deno.env.get("MEOWERTESTUSER"), undefined);
+    assertNotEquals(Deno.env.get("MEOWERTESTPW"), undefined);
+  });
+
